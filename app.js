d5f63c84fae8859b1a4d542d0d170606c9fc8674
Leehaney gee <georgeleehaney20@gmail.com>
2022-11-29 15:37:46 +0300

    Feat: Add app file

--- app.js
+++ app.js
@@ -1,75 +1,11 @@
-// select the form
+//select the items
 const form = document.querySelector('#postBook');
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const displayArea = document.querySelector('#showBooks');
 
-// Display the books stpr
-function displayBooks() {
-  const wrapper = document.createElement('div');
-  const bookShelfstr = localStorage.getItem('books');
-  const bookArray = JSON.parse(bookShelfstr);
-  bookArray.forEach((element, index) => {
-    const displayTitle = document.createElement('p');
-    const displayAuth = document.createElement('p');
-    const deletebtn = document.createElement('div');
-    const container = document.createElement('div');
-    const line = document.createElement('hr');
-    displayTitle.innerText = element.title;
-    displayAuth.innerText = element.author;
-    deletebtn.innerHTML = `<button onclick='removeBook(${index})'>Remove</button>`;
-    deletebtn.classList.add('deleteBook');
-    container.appendChild(displayTitle);
-    container.appendChild(displayAuth);
-    container.appendChild(deletebtn);
-    container.appendChild(line);
-    wrapper.appendChild(container);
-  });
-  displayArea.appendChild(wrapper);
-}
+/* Talha here */
 
-/* Take the data store it  */
-const storeObj = {};
 
-// Function to store in local storage
-function storeData() {
-  // Check if local storage is empty
-  if (localStorage.getItem('books') === null) {
-    const bookShelf = [];
-    bookShelf.push(storeObj);
-    localStorage.setItem('books', JSON.stringify(bookShelf));
-  } else {
-    const bookShelfstr = localStorage.getItem('books');
-    const bookArray = JSON.parse(bookShelfstr);
-    bookArray.push(storeObj);
-    localStorage.setItem('books', JSON.stringify(bookArray));
-  }
-  // Clear input
-  title.value = '';
-  author.value = '';
-  displayBooks();
-}
+/*Steve here  */
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  storeObj.title = title.value;
-  storeObj.author = author.value;
-  displayArea.innerHTML = '';
-  storeData();
-});
-
-// remove a book
-function removeBook(index) {
-  const bookShelfstr = localStorage.getItem('books');
-  const bookArray = JSON.parse(bookShelfstr);
-  bookArray.splice(index, 1);
-  localStorage.setItem('books', JSON.stringify(bookArray));
-  displayArea.innerHTML = '';
-  displayBooks();
-}
-
-window.addEventListener('load', displayBooks);
-
-if ('cl' === 'clz') {
-  removeBook(1);
-}
