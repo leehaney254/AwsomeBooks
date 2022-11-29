f192c13c129fe0631f09f4e62caf122688faca50
Talha-Hanif5666 <talhahanif5666@gmail.com>
2022-11-29 17:41:25 +0500

    Add class funtion in javascript file

--- app.js
+++ app.js
@@ -4,8 +4,62 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const displayArea = document.querySelector('#showBooks');
 
-/* Talha here */
-
+class Books {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+  storeData() {
+    const bookTitle = title.value;
+    const bookAuthor = author.value;
+    const book = new Books(bookTitle, bookAuthor);
+    // Check if local storage is empty
+    if (localStorage.getItem('books') === null) {
+      const bookShelf = [];
+      bookShelf.push(book);
+      localStorage.setItem('books', JSON.stringify(bookShelf));
+    } else {
+      const bookShelfstr = localStorage.getItem('books');
+      const bookArray = JSON.parse(bookShelfstr);
+      bookArray.push(book);
+      localStorage.setItem('books', JSON.stringify(bookArray));
+    }
+    // Clear input
+    title.value = '';
+    author.value = '';
+    this.displayBooks();
+  }
+  displayBooks() {
+    const wrapper = document.createElement('div');
+    const line = document.createElement('hr');
+    const bookShelfstr = localStorage.getItem('books');
+    const bookArray = JSON.parse(bookShelfstr);
+    bookArray.forEach((element, index) => {
+      const displayTitle = document.createElement('p');
+      const displayAuth = document.createElement('p');
+      const deletebtn = document.createElement('div');
+      const container = document.createElement('div');
+      displayTitle.innerText = element.title;
+      displayAuth.innerText = element.author;
+      deletebtn.innerHTML = `<button onclick='deleteItem(${index})'>Remove</button>`;
+      deletebtn.classList.add('deleteBook');
+      container.appendChild(displayTitle);
+      container.appendChild(displayAuth);
+      container.appendChild(deletebtn);
+      wrapper.appendChild(container);
+    });
+    displayArea.appendChild(wrapper);
+    displayArea.appendChild(line);
+  }
+  removeBook(index) {
+    const bookShelfstr = localStorage.getItem('books');
+    const bookArray = JSON.parse(bookShelfstr);
+    bookArray.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(bookArray));
+    displayArea.innerHTML = '';
+    this.displayBooks();
+  }
+}
 
 /*Steve here  */
 
